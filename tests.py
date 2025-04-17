7bf68f482e57a0376f8191dd943f6b8f2ea11466
mjbaker474 <mjbaker474@gmail.com>
2025-04-17 08:49:01 -0500

    Initial commit

---
+++ tests.py
@@ -0,0 +1,18 @@
+# Author: Matthew Baker
+# Github: mjbaker474
+# Email: bakerma2@oregonstate.edu
+# Assignment: CS362 HW 1
+# Description: Black Box test file for credit card validator.
+
+import unittest
+from credit_card_validator import credit_card_validator
+
+class TestCreditCardValidator(unittest.TestCase):
+    """Unit tests for credit_card_validator function."""
+    
+    def test_1(self):
+        pass
+
+if __name__ == 'main':
+    unittest.main()
+    
